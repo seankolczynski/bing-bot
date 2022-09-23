f91cb3522dc851dd6b99711d6f483b6a366b3c5c
seankolczynski <sean.kolczynski@gmail.com>
2022-09-23 00:30:46 -0400

    randomizer

--- searchGenerator.py
+++ searchGenerator.py
@@ -1,9 +1,12 @@
+import random
+
 def searchGenerator(count):
+    search = ""
+    random.randint(1, count - 1)
     with open("Genie.txt") as file:
         lineNum = 0
         for line in file:
             if (count == lineNum):
                 search = line
             lineNum = lineNum + 1
-    print(search)
     return search
