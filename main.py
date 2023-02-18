f81e21bcd53bc6b74d747792bd5f1ec9be3ca1a7
Sean Kolczynski <44072265+seankolczynski@users.noreply.github.com>
2023-02-18 14:19:17 -0500

    sets up directories

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from searchGenerator import searchGenerator, searchMonkey, searchMobile
 from signIn import signIn
-from selenium import webdriver
+from setup import setup
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -9,6 +9,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    setup()
     searchGenerator()
     driver = signIn()
     driver = searchMonkey(driver)
